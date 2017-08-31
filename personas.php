35c424b10e383bc517f9ef22a9245fdadbde3f7e
jonathan <jonathanmix2016@hotmail.com>
2017-08-31 16:29:26 -0300

    clase2

---
+++ personas.php
@@ -0,0 +1,26 @@
+<?php
+class personas {
+	
+	  public $nombre;
+	  public $apellido;
+	  
+	 function hablar(){
+		
+		
+	}
+
+  
+    	
+	 
+}
+
+$p=new personas();
+$p->nombre= "micaela";
+$p->apellido="baravalle";
+
+$p1->nombre="juan";
+$p1->apellido="peres";
+
+
+echo $p->nombre;
+
